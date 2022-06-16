7a3c41c3c5445aedbc2dbeeb8164ebca761844f6
Ochanyoro <wadaoo0223@icloud.com>
2022-06-17 00:00:28 +0900

    create connecting ViewController and label

---
+++ relationshipBTstoryandprg/ThirdViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ThirdViewController.swift
+//  relationshipBTstoryandprg
+//
+//  Created by 大和田一裕 on 2022/06/16.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    @IBOutlet weak var label3: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        label3.text = "３つ目の画面"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
